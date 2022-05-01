ccc6c30e0edcb1d347540bc25a8c4ae71562b589
Alyssa Voccia <avoccia@hotmail.com>
2022-05-01 15:05:52 -0400

    Working on updating store page

--- src/pages/store/Store.jsx
+++ src/pages/store/Store.jsx
@@ -1,11 +1,56 @@
-import './store.scss';
+import { useState, useEffect } from 'react';
 import Products from '../../components/shopify/products/Products';
 import '../../global.scss';
+import './store.scss';
 
 function Store() {
+  const [selected, setSelected] = useState('all');
+
+  const list = [
+    {
+      id: 'all',
+      title: 'All'
+    },
+    {
+      id: 'wood',
+      title: 'Wood Pieces'
+    },
+    {
+      id: 'custom',
+      title: 'Custom Pieces'
+    },
+    {
+      id: 'plans',
+      title: 'Plans'
+    },
+    {
+      id: 'tools',
+      title: 'Tools'
+    },
+    {
+      id: 'apparel',
+      title: 'Apparel'
+    },
+    {
+      id: 'accessories',
+      title: 'Accessories'
+    }
+  ];
+
+  useEffect(() => {
+    switch (selected) {
+
+    }
+  }, [selected]);
+
   return (
     <div className='store container'>
       <h1 className='section-title'>Store</h1>
+      <ul className='store__categories'>
+        {list.map(item => (
+          <li key={item.title} className={`store__options ${item.id === selected && 'active'}`} onClick={() => setSelected(item.id)}>{item.title}</li>
+        ))}
+      </ul>
       <Products />
     </div>
   )
