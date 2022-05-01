0cdff4a1ff60d5b280d7607799086208c0a8eb25
Alyssa Voccia <avoccia@hotmail.com>
2022-05-01 19:07:13 -0400

    Updated the shop page

--- src/pages/the-shop/TheShop.jsx
+++ src/pages/the-shop/TheShop.jsx
@@ -1,3 +1,6 @@
+import Shop1 from '../../assets/shop1.jpg';
+import Shop2 from '../../assets/shop2.jpg';
+import Shop3 from '../../assets/shop3.jpg';
 import '../../global.scss';
 import './theshop.scss';
 
@@ -5,6 +8,21 @@ function TheShop() {
   return (
     <div className='the-shop'>
       <h1 className="section-title">The Shop</h1>
+      <div className="container">
+        <div className="the-shop__contents">
+          <div className="the-shop__images">
+            <img src={Shop1} alt="Shop" />
+            <img src={Shop2} alt="Shop" />
+            <img src={Shop3} alt="Shop" />
+          </div>
+          <div className="the-shop__favorites">
+            <h2 className="section-subtitle">Tools In The Shop</h2>
+          </div>
+          <div className="the-shop__recommendations">
+            <h2 className="section-subtitle">Other Tools I Recommend</h2>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
